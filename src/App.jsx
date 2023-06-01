14f47f5b530feff8687ddc4999f1eec955503ad6
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-06-01 17:59:19 +0200

    routes update2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
