640139ba7ece9da1008370860ab8a22d97082c0b
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-06-15 11:10:41 +0200

    basename in browser router added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/My-Portfolio">
     <App />
   </BrowserRouter>
 );
