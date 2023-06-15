94abe60050fcf2df719196979c40a6d263737a47
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-06-15 11:17:56 +0200

    routes added in main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,16 +3,18 @@ import Home from "../Home/Home.jsx";
 import About from "../About/About.jsx";
 import Projects from "../Projects/Projects.jsx";
 import Contact from "../Contact/Contact.jsx";
+import { Navigate } from "react-router-dom";
 
 const Main = () => {
   return (
     <div className="main">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
